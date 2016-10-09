b277eeb78fa69081174b54234d745b41c494dcc1
devopsec <tyler.moore58@gmail.com>
2016-10-08 21:09:50 -0400

    fixed bugs found in testing: -fixed packaging issues - fixed import / directory path dependency issue, -fixed json parsing issue -fixed update.py logic issue and created time conversion function -deleted placeholder and unused code -added server POST information capabilities via curl -added cron app timing to network tap -replaced GET call device name with variable hostname (make sure hostname on agents are correctly named or an error will be thrown) Bugs found that need addressed: Critical - host server can not run metron vm and api at the same time (they bind to one of the same ports using socket, may be a tcp port, probably port 80), Minimal - need to add logging and error reporting to client scripts and forward via email, Moderate - need to add input checking to client and server scripts and either handle error or run an alternate script. Possible fix for server hosting issue could be changing port that socket binds to in code or creating script to shutdown api and start metron when replay is going to execute.

--- agents/PiTap/rpi/update.py
+++ agents/PiTap/rpi/update.py
@@ -4,20 +4,27 @@ and syncs clock with host server
 @author: devopsec
 '''
 
-import subprocess, requests, sys
 sys.path.insert(0, ("/threatdetectionservice/agents/PiTap/"))
+import subprocess, requests, sys, json
 import datetime, socket
 from rpi import EnableCapture
 
-start, end, cmd = ""
-#chk = False
+#define convtime func
+def convTime(tStr):
+    t0 = tStr.split(':')
+    time0 = datetime.timedelta(hours=int(t0[0]), minutes=int(t0[1]), seconds=int(t0[2]))
+    return time0
+
+start=end=cmd=""
 r1 = requests.get("http://10.10.10.154:6668/api/picontroller/time")
-t = r1.text
-subprocess.Popen('timedatectl', 'set-time', t)
+t = json.loads(r1.text)
+subprocess.Popen(['timedatectl', 'set-time', t], bufsize=0)
+temp = t.split(' ')
+t = temp[1]
 
 #variable hostname, ensure hostname is set correctly on device
 r2 = requests.get("http://10.10.10.154:6668/api/picontroller/" + socket.gethostname())
-cmds = r2.text
+cmds = json.loads(r2.text)
 if cmds['start'] != "":
     start = cmds['start']
 if cmds['end'] != "":
@@ -51,19 +58,5 @@ if cmds['cmd'] != "":
         from rpi import RestartPi
         RestartPi()
 
-#define convtime func
-def convTime(tStr):
-    t0 = tStr
-    t0 = t0.split(' ')
-    t0 = t0[1].split(':')
-    time0 = datetime.timedelta(hours=int(t0[0]), minutes=int(t0[1]), seconds=int(t0[2]))
-    return time0  
-
 sys.exit(0)
 
-        
-
-
-        
-        
-    
