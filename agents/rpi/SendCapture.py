1d953f9891596bb9085e1d1105fd08e11214861c
mack <mack@mack.dopensource.com>
2016-11-01 13:17:33 -0400

    Updated Kafka settings to allow it to be accessed remotely

--- agents/rpi/SendCapture.py
+++ agents/rpi/SendCapture.py
@@ -1,12 +1,12 @@
 from kafka import KafkaProducer
 
 #producer = KafkaProducer(bootstrap_servers=['10.10.10.154:6667'],api_version=(0,9))
-producer = KafkaProducer(bootstrap_servers='10.10.10.154:6667')
-#for _ in range(100):
-#    producer.send('pcap', b'some_message_bytes')
+producer = KafkaProducer(bootstrap_servers='50.253.243.17:6667')
+for _ in range(100):
+    producer.send('asset', b'some_message_bytes')
 
     # Block until all pending messages are sent
-#    producer.flush()
+    producer.flush()
     # Block until a single message is sent (or timeout)
 
 
