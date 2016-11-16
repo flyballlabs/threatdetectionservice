62d939c6b565d6922e93348b85bfcfd72f85adb7
devopsec <tyler.moore58@gmail.com>
2016-11-15 19:03:42 -0500

    made improvements to api, added sql alchemy compatibility for flask

--- api/rest-server.py
+++ api/rest-server.py
@@ -1,8 +1,8 @@
-#!/flask/bin/python
+#!/usr/bin/python3.5
 
 from flask import Flask, request, jsonify#, abort, make_response, render_template
 from flask_restful import Api, Resource, reqparse, fields#, marshal
-from _ast import Str
+from flask_sqlalchemy import SQLAlchemy
 from array import array
 from datetime import datetime, timezone, timedelta
 import os
@@ -10,28 +10,51 @@ import os
 
 #auth = HTTPBasicAuth()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://tmp:tmp@127.0.0.1/tmp'
+# Create SQLAlchemy object
+db = SQLAlchemy(app)
+# Create API object
 api = Api(app)
 
-'''http auth
-auth = HTTPBasicAuth()
+class user(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), unique=True)
+    firstname = db.Column(db.String(45))
+    lastname = db.Column(db.String(45))
+    password = db.Column(db.String(45))
+    email = db.Column(db.String(45), unique=True)
+    company_id = db.Column(db.String(45))
+    status = db.Column(db.String(45))
+    lastlogin = db.Column(db.String(45))
 
+    def __init__(self, user_id, username, firstname, lastname, password, email, company_id, status, lastlogin):
+        self.user_id = user_id
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.password = password
+        self.email = email
+        self.company_id = company_id
+        self.status = status
+        self.lastlogin = lastlogin
 
-@auth.get_password
-def get_password(username):
-    if username == 'miguel':
-        return 'python'
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    # return 403 instead of 401 to prevent browsers from displaying the default
-    # auth dialog
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+    def __repr__(self):
+        return '{user: %r}' % self.user_id
 '''
-
-
-
+user_data = {
+    'id': {
+        'bob1234@gmail.com': {
+            'pw': '1234'
+        },
+        'jessica1234@yahoo.com': {
+            'pw': '1234'
+        },
+        'fred1234@gmail.com': {
+            'start': '1234'
+        }
+    }
+}
+'''
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
@@ -40,17 +63,6 @@ def bad_request(error):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
-#@app.route('/api/interface', methods=['GET'])
-#def interface():
-#    return render_template("interface.html", title = 'interface')
-
-#@app.route('/api/picontroller', methods=['POST', 'GET'])  
-#def get(self, id):
-#        if id in data['id']:
-#    return {'start': data['id'][id]['start'],'end': data['id'][id]['end'],'cmd': data['id'][id]['cmd']}
-
-
 
 data = {
     'id': {
@@ -69,28 +81,8 @@ data = {
             'end': '16:00:00',
             'cmd': ''
         }
-    },
+    }
 }
-
-#status = {
-#    'id': {
-#        'glazer': {
-#            'online': True,
-#            'time': '2016-10-05_22:52:13',
-#            'rules': '[]'
-#        },
-#        'loving': {
-#            'online': True,
-#            'time': '2016-10-05_22:52:13',
-#            'rules': '[]'
-#        },
-#        'school3': {
-#            'online': True,
-#            'time': '2016-10-05_22:52:13',
-#            'rules': '[]'
-#        }
-#    },
-#} 
 
 
 class timeSync(Resource):
@@ -112,10 +104,21 @@ class update(Resource):
         data['id'][id]['cmd'] = cmd
         return 'UPDATED VALUES For:' + id + os.linesep + 'START:' + start + os.linesep + 'END:' + end + os.linesep + 'CMD:' + cmd
 
+class user_auth(Resource):
+    def get(self, user_name, user_pw):
+        x = user.query.filter_by(username=user_name)
+        if x != None:
+            if x.password == user_pw:
+                return {'authentication': True}
+            else:
+                return {'authentication': False}
+        else:
+            return {'error': 'Not a valid username'}
+
 api.add_resource(timeSync, '/api/picontroller/time')
 api.add_resource(piController, '/api/picontroller/<string:id>')
 api.add_resource(update, '/api/picontroller/<string:id>/<string:start>/<string:end>/<string:cmd>')
-
+api.add_resource(user_auth, '/api/auth/<string:user_name>/<string:user_pw>')
 
 if __name__ == '__main__':
-    app.run(host='10.10.10.154', port=6668, debug=False)
+    app.run(host='10.10.10.142', port=6668, debug=False)
---
+++ api/sql/tmp.sql
@@ -0,0 +1,66 @@
+-- Quick and dirty way to create a database and setup a local and remote user
+
+CREATE DATABASE IF NOT EXISTS `tmp`;
+grant all on tmp.* TO 'tmp'@'localhost' IDENTIFIED BY 'tmp';
+grant all on tmp.* TO 'tmp'@'%' IDENTIFIED BY 'tmp';
+flush privileges;
+use tmp;
+
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: tmp
+-- ------------------------------------------------------
+-- Server version	5.1.73-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `company_id` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `lastlogin` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'mack@goflyball.com','Mack','Hendricks','flyball','mack@goflyball.com','1','1',NULL),(2,'tmoore@goflyball.com','Tyler','Moore','flyball','tmoore@goflyball.com','1','1',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-15 16:12:24
