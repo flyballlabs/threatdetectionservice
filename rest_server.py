0abd2c0b09e41c8f7e65d642d897af092da36a31
Tyler Moore <tyler.moore58@gmail.com>
2016-12-20 18:23:54 -0500

    updated user api functions
    for review

--- rest_server.py
+++ rest_server.py
@@ -5,114 +5,46 @@
 Ensures all submodules are added to sys path and configures all runtime settings.
 @author devopsec
 '''
-import os
-from datetime import datetime
-from api import app;
-from flask import Flask, make_response, jsonify, g, url_for ,abort, request
-from flask_restful import Api  #, marshal, Resource, reqparse, fields
-from werkzeug.serving import run_simple
-from flask_cors import CORS, cross_origin
-from flask_httpauth import HTTPBasicAuth
-from passlib.apps import custom_app_context as pwd_context
 
-from api.database import *
-from api.sql.models import *
+from app import *
+from flask import Flask, make_response, jsonify #, request, abort, render_template, Flask
+from flask_restful import Api, reqparse, fields #, marshal, Resource
+from flask_cors import CORS, cross_origin
 
 # import endpoints #
-from api.auth.endpoint import userAuth
-from api.user.endpoint import manageUsers
-from api.agent.endpoint import manageAgents, piController
-from api.company.endpoint import manageCompany, companyList
-#from api.notification.endpoint import manageNotifications
-from api.asset.endpoint import manageAssets
-from api.metron.endpoint import metronThreats
-#from api.metron_data.endpoint import asset_discovery, threat_intel
+from auth.endpoint import *
+from user.endpoint import *
+from agent.endpoint import *
+from company.endpoint import *
+from metron.endpoint import *
+from asset.endpoint import *
 
 # flask / sql / api config  #
 app = Flask('rest_server')
-#app.config['DEBUG'] = False
+app.config['DEBUG'] = True
 api = Api(app)
 CORS(app)
-auth = HTTPBasicAuth()
-
-
-
-#for login page, USER is session object for auth user
-@auth.verify_password
-def verify_password(username_or_token, password):
-    # first try to authenticate by token
-    USER = user_data.verify_auth_token(username_or_token)
-    if not USER:
-        # try to authenticate with username/password
-        USER = user_data.query.filter_by(username=username_or_token).first()
-        if not USER or not user_data.verify_password(password):
-            return False
-    g.user_data = USER
-    return True
-
-#redir at here to get a token#
-@app.route('/api/auth/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token(600)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
-
-#verify to protect resources
-@app.route('/api/resource')
-@auth.login_required
-def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.username})
-        
-        
-@app.route('/api/users/<int:id>')
-def get_userID(id):
-    USER = user_data.query.get(id)
-    if not USER:
-        abort(400)
-    return jsonify({'username': USER.username})
-
-
-
-'''# hashing functions #
-@auth.hash_password
-def hash_pw(password):
-    return md5(password).hexdigest()
-
-@auth.hash_password
-def hash_pw(username, password):
-    get_salt(username)
-    return hash(password, salt)
-
-@auth.verify_password
-def verify_pw(username, password):
-    return call_custom_verify_function(username, password)'''
-
 
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-
+api.add_resource(manageAgents, '/api/agent', '/api/agent/<string:_mac_address_>')
+#api.add_resource(timeSync, '/api/picontroller/time')
+#api.add_resource(piController, '/api/picontroller/<string:id>')
+#api.add_resource(update, '/api/picontroller/<string:id>/<string:start>/<string:end>/<string:cmd>')
+api.add_resource(userAuth, '/api/auth/<string:_username>/<string:_password>')
+api.add_resource(manageUser, '/api/user/<string:_username_>')
+api.add_resource(manageUserList, '/api/user/list')
+api.add_resource(manageCompany, '/api/company/<string:_company_name_>')
+api.add_resource(companyList, '/api/company', '/api/company/sites', '/api/company/<string:_company_name_>/sites')
 api.add_resource(metronThreats, '/api/metron/threats/<string:_device_>')
 api.add_resource(manageAssets, '/api/assets/<string:_device_>')
 
-api.add_resource(manageAgents, '/api/agent', '/api/agent/<string:_mac_address_>')
-api.add_resource(piController, '/api/picontroller', '/api/picontroller/<string:_mac_address_>')
-api.add_resource(userAuth, '/api/auth/<string:_username>/<string:_password>')
-api.add_resource(manageUsers, '/api/user', '/api/user/<string:_username_>')
-api.add_resource(manageCompany, '/api/company/<string:_company_name_>')
-api.add_resource(companyList, '/api/company', '/api/company/sites', '/api/company/<string:_company_name_>/sites')
-#api.add_resource(manageNotifications, '/api/notification', '/api/notification/<string:_username_>')
-api.add_resource(manageAssets, '/api/metron_data/asset_discovery/<string:_company_name_>', '/api/metron_data/asset_discovery/<string:_company_name_>/<string:_sites_>', '/api/metron_data/asset_discovery/<string:_company_name_>/<string:_sites_>/<string:_mac_address_>')
-#api.add_resource(threat_intel, '/api/metron_data/threat_intel', '/api/metron_data/threat_intel/<string:_company_name_>', '/api/metron_data/threat_intel/<string:_company_name_>/<string:_sites_>')
-                 
 if __name__ == '__main__':
-    if not os.path.exists('db.mysql'):
-        db = connect()
-        db.create_all()
-    app.run(host='0.0.0.0', port=7777, debug=True)
+    app.run(host='0.0.0.0', port=7777)
---
+++ user/endpoint.py
@@ -0,0 +1,201 @@
+from flask_restful import Resource, reqparse
+from sql.models import *  #import all of the models from models.py
+#from app.parse_json import * #for json request parsing
+
+class manageUser(Resource):
+    def get(self, _username_):
+        try:
+            x = user_data.query.filter_by(username=_username_).first()
+            _user_id = x.user_id
+            _firstname = x.firstname
+            _lastname = x.lastname
+            _email = x.email
+            _company_id = x.company_id
+            _status = x.status
+            _lastlogin = x.lastlogin
+            
+            if x != None:
+                return {
+                        'user_id':_user_id,
+                        'username':_username_,
+                        'firstname' : _firstname,
+                        'lastname' : _lastname,
+                        'email' : _email,
+                        'company_id' : _company_id,
+                        'status' : _status,
+                        'lastlogin' : _lastlogin,
+                        'message':'User search success'
+                       }
+            else:
+                return {
+                        'status': 400,
+                        'message':'User search failure'
+                       }
+        except Exception as e:
+            return {'error': str(e)}
+    
+    def put(self, _username_):
+        try:
+            parser = reqparse.RequestParser()
+            
+            # conditionally replace user data if arg exists #
+            parser.add_argument('user_id', type=int, help='Username for account', location='json')
+            parser.add_argument('username', type=str, help='Password for account', location='json')
+            parser.add_argument('firstname', type=str, help='Firstname for account', location='json')
+            parser.add_argument('lastname', type=str, help='Lastname for account', location='json')
+            parser.add_argument('password', type=str, help='Password for account', location='json')
+            parser.add_argument('email', type=str, help='Email for account', location='json')
+            parser.add_argument('company_id', type=str, help='Company_id for account', location='json')
+            parser.add_argument('status', type=str, help='Status for account', location='json')
+            parser.add_argument('lastlogin', type=str, help='Lastlogin for account', location='json')
+            
+            args = parser.parse_args()#strict=True, require=True
+            
+            #for response in args:         ############################  
+            #    if args[response] != None:# optimize in future release
+                
+            if args['user_id'] != None:
+                _user_id = args['user_id']
+            if args['username'] != None:
+                _username = args['username']
+            if args['firstname'] != None:
+                _firstname = args['firstname']
+            if args['lastname'] != None:
+                _lastname = args['lastname']
+            if args['password'] != None:
+                _password = args['password']
+            if args['email'] != None:
+                _email = args['email']
+            if args['company_id'] != None:
+                _company_id = args['company_id']
+            if args['status'] != None:
+                _status = args['status']
+            if args['lastlogin'] != None:
+                _lastlogin = args['lastlogin']
+            ###################################
+            # would be faster in an array / loop
+            
+            try:
+                curr_session = db.session #open database session
+                x = user_data.query.filter_by(username=_username_).first() #fetch the username to be updated
+                x.user_id = _user_id   #update the row
+                x.username = _username
+                x.firstname = _firstname
+                x.lastname = _lastname
+                x.password = _password
+                x.email = _email
+                x.company_id = _company_id
+                x.status = _status
+                x.lastlogin = _lastlogin
+                curr_session.commit() #commit changes
+                
+                return  {
+                            'status': 200,
+                            'message':'User update successful'
+                        }
+            except:
+                curr_session.rollback()
+                curr_session.flush() # for resetting non-commited .add()
+                return  {
+                            'status':400,
+                            'message':'User update failure'
+                        }
+        except Exception as e:
+            return {'error': str(e)}
+        
+    def delete(self, _username_):
+        try:
+            curr_session = db.session #open database session
+            x = user_data.query.filter_by(username=_username_).first()
+            try:
+                db.session.delete(x)
+                db.session.commit()
+                return  {
+                            'status': 200,
+                            'message':'User delete successful'
+                        }
+            except:
+                curr_session.rollback()
+                curr_session.flush() # for resetting non-commited .add()
+                return  {
+                            'status':400,
+                            'message':'User delete failure'
+                        }
+        except Exception as e:
+            return {'error': str(e)}
+
+class manageUserList(Resource):
+    def get(self):
+        try:
+            x = user_data.query.all()
+            if x != None:
+                results = []
+                for user in x:
+                    results.append({
+                    'user_id':user.user_id,
+                    'username':user.username,
+                    'firstname' : user.firstname,
+                    'lastname' : user.lastname,
+                    'email' : user.email,
+                    'company_id' : user.company_id,
+                    'status' : user.status,
+                    'lastlogin' : user.lastlogin
+                    })
+                            
+                return {
+                        'message':'User search success',
+                        'users':results
+                       }
+            else:
+                return {
+                        'status': 400,
+                        'message':'User search failure'
+                       }
+        except Exception as e:
+            return {'error': str(e)}
+    
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('user_id', type=int, help='User_id for account', location='json')
+            parser.add_argument('username', type=str, help='Username for account', location='json')
+            parser.add_argument('firstname', type=str, help='Firstname for account', location='json')
+            parser.add_argument('lastname', type=str, help='Lastname for account', location='json')
+            parser.add_argument('password', type=str, help='Password for account', location='json')
+            parser.add_argument('email', type=str, help='Email for account', location='json')
+            parser.add_argument('company_id', type=str, help='Company_id for account', location='json')
+            parser.add_argument('status', type=str, help='Status for account', location='json')
+            parser.add_argument('lastlogin', type=str, help='Lastlogin for account', location='json')
+            args = parser.parse_args()#strict=True
+
+            _user_id = args['user_id']
+            _username = args['username']
+            _firstname = args['firstname']
+            _lastname = args['lastname']
+            _password = args['password']
+            _email = args['email']
+            _company_id = args['company_id']
+            _status = args['status']
+            _lastlogin = args['lastlogin']
+            
+            query = user_data(user_id=_user_id, username=_username, firstname=_firstname, 
+                              lastname=_lastname, password=_password, email=_email, 
+                              company_id=_company_id, status=_status, lastlogin=_lastlogin)
+
+            curr_session = db.session #open database session
+            try:
+                curr_session.add(query) #add prepared statement to opened session
+                curr_session.commit() #commit changes
+                return  {
+                            'status': 200,
+                            'message':'User creation successful'
+                        }
+            except:
+                curr_session.rollback()
+                curr_session.flush() # for resetting non-commited .add()
+                return  {
+                            'status': 400,
+                            'message':'User creation failure'
+                        }
+        except Exception as e:
+            return {'error': str(e)}
