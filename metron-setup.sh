b425917afddb193999a068f2fb632045e02adb61
mack <mack@mack.dopensource.com>
2017-01-02 15:20:34 -0500

    Framework for a setup script to configure a Metron Vagrant VM setup

---
+++ metron-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: devopsec and mackhendricks
+# Summary: This deployment script is focused on configuring a metron environment 
+# with the configs and programs needed by the Threat Mananagement Platform API to work
+# properly
+# Note: This script must be run as root user to work properly.
+# Note: Platform must be a unix-based x64 architecture os.
+
+# Version 1.0
+# - Focused on working with Metron Vagrant Install
+
+### Sudo Code ###
+
+#Check if the Vagrant VM of Metron is installed and up
+
+#Find out which version of Metron is installed
+
+#Create a directory for TMP Specific stuff
+
+#Copy over TMP files
+
+#Load configs into Storm 
+
+#Config Kafka as needed
+
+#Setup SFTP landing zones for PCAP files
