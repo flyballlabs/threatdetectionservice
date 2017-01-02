f36c79354a9ae40b6acdc5da4dcb21b469b6ed39
Mack Hendricks <mack@goflyball.com>
2017-01-02 18:16:08 -0500

    The script can send over a Test file to the Vagrant VM

--- metron-setup.sh
+++ metron-setup.sh
@@ -14,6 +14,36 @@
 
 #Check if the Vagrant VM of Metron is installed and up
 
+# This can be the Vagrant file for the Quick Dev Install, Code Lab, Full Install, etc
+# If this is not set, we will prompt for the location
+
+METRON_VAGRANT_DIR=/incubator-metron/metron-deployment/vagrant/quick-dev-platform
+
+if [ -z "${METRON_VAGRANT_DIR+x}" ]; then
+
+	echo "Please provide the directory that contains your Metron Vagrant File"
+	read METRON_VAGRANT_DIR
+fi
+
+echo "Vagrant Directory Used: $METRON_VAGRANT_DIR"
+
+# Use the built-in environment variable to specify the Vagrantfile to use
+# when 
+
+VAGRANT_VAGRANTFILE=$METRON_VAGRANT_DIR/Vagrantfile
+
+echo "Vagrant File Used: $VAGRANT_VAGRANTFILE"
+cd $METRON_VAGRANT_DIR
+SSH_PRIVATE_KEY=`vagrant ssh-config | grep IdentityFile | awk '{print $2}'|sed 's/\"//g'`
+echo "Vagrant SSH_PRIVATE_KEY:$SSH_PRIVATE_KEY"
+
+scp -P 2222 -i $SSH_PRIVATE_KEY ~/test.txt vagrant@127.0.0.1:.
+if [ $? -eq 0 ]; then
+
+	echo "Test file was sent over"
+fi
+
+
 #Find out which version of Metron is installed
 
 #Create a directory for TMP Specific stuff
