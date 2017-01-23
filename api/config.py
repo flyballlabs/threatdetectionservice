11dd781e05a743c1b7e9309eb71c0a34ab4e6c55
mack <mack@mack.dopensource.com>
2017-01-23 06:18:29 -0500

    Added the framework facial recognition endpoint

---
+++ api/config.py
@@ -0,0 +1,6 @@
+#Facial Recognition Configuration Parameters
+
+FACIAL_ENGINES = "MSFT"
+FACIAL_MSFT_API_KEY = "e0ace679238e4af9a6217f460a1378d5" 
+FACIAL_IBM_API_KEY =
+FAICAL_INTERNAL_API_KEY =
---
+++ api/facial/endpoint.py
@@ -0,0 +1,3 @@
+from flask_restful import Resource, reqparse
+from api.sql.models import *  #import all of the models from models.py
+from api import *
